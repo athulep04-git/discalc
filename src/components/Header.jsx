6d2133649bcb96ca88e3603867fd7676f14bca96
athulep04-git <athulep04@gmail.com>
2025-10-31 16:12:19 +0530

    error fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,20 +7,20 @@ function Header() {
       <Container>
         <div className="d-flex align-items-center gap-2">
           <img src="https://th.bing.com/th/id/R.1a551aa4cba59342dff2decfbaa9c8dd?rik=KQTDhx1ZUAAfjA&riu=http%3a%2f%2fpluspng.com%2fimg-png%2flogo-template-png-logo-templates-1655.png&ehk=9MRokJPqMM6lr6AsMn50qqBGQgGuPYXFuTzMFbKjOa8%3d&risl=&pid=ImgRaw&r=0" alt="logo" width="40" className="rounded-circle shadow-sm" />
-          <Navbar.Brand href="/" className="fw-bold fs-4 text-info">
+          <Navbar.Brand to="/" className="fw-bold fs-4 text-info">
             DISCALC
           </Navbar.Brand>
         </div>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className=" d-flex align-items-center gap-4">
-            <Nav.Link href="/" className="text-light ">
+            <Nav.Link to="/" className="text-light ">
               Home
             </Nav.Link>
-            <Nav.Link href="/discount" className="text-light ">
+            <Nav.Link to="/discount" className="text-light ">
               Calculator
             </Nav.Link>
-            <Nav.Link href="/movies" className="text-light ">
+            <Nav.Link to="/movies" className="text-light ">
               Movies
             </Nav.Link>
           </Nav>
